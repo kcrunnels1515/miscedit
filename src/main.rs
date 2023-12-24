d07cacd8c0a34f3f91929c60c666f144978bc7e3
Kelly Laptop <kellyr@kelly-latitudee6430>
2023-12-24 10:46:12 -0500

    concatenate display string

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use crossterm::{
     event::{poll, read, Event, KeyEvent, KeyCode, KeyModifiers},
 };
 
-fn poll_insert(text: &mut Vec<char>, b_gap: &mut usize, e_gap: &mut usize, code: KeyCode, modifiers: KeyModifiers) -> io::Result<()> {
+fn poll_insert(text: &mut Vec<char>, b_gap: &mut usize, e_gap: &mut usize, code: KeyCode, modifiers: KeyModifiers) -> io::Result<bool> {
+    let mut text_changed: bool = false;
     match code {
         KeyCode::Char(chr) => {
             if chr == 'c' && modifiers == KeyModifiers::CONTROL {
@@ -16,12 +17,14 @@ fn poll_insert(text: &mut Vec<char>, b_gap: &mut usize, e_gap: &mut usize, code:
             } else {
                 text[*b_gap] = chr;
                 *b_gap += 1;
+                text_changed = true;
             }
         },
         KeyCode::Backspace => {
             if *b_gap > 0 {
                 //text[*b_gap - 1] = '\0';
                 *b_gap -= 1;
+                text_changed = true;
             }
         },
         KeyCode::Left => {
@@ -42,7 +45,11 @@ fn poll_insert(text: &mut Vec<char>, b_gap: &mut usize, e_gap: &mut usize, code:
         },
         _ => {},
     }
-    Ok(())
+    Ok(text_changed)
+}
+
+fn buf_to_string(gap_buf: &Vec<char>, b_gap: usize, e_gap: usize) -> String {
+    String::from_iter(&gap_buf[0..b_gap]) + &String::from_iter(&gap_buf[e_gap..(gap_buf.len() - 1)])
 }
 
 fn main() -> std::io::Result<()> {
@@ -50,13 +57,21 @@ fn main() -> std::io::Result<()> {
     enable_raw_mode()?;
     let mut e_gap: usize = 49;
     let mut b_gap: usize = 0;
+    let mut dis_str: String = String::new();
     loop {
         if poll(Duration::from_millis(500))? {
             if let Event::Key(KeyEvent { code, modifiers, ..}) = read()? {
-                if poll_insert(&mut text, &mut b_gap, &mut e_gap, code, modifiers).is_err() {
+                let result: io::Result<bool> = poll_insert(&mut text, &mut b_gap, &mut e_gap, code, modifiers);
+                if result.is_err() {
                     break;
                 }
+                if let Ok(change) = result {
+                    if change {
+                        dis_str = buf_to_string(&text,b_gap,e_gap);
+                    }
+                }
             }
+
         }
     }
     println!("{:?}", text);
